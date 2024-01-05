bd94ef11e57300c0f213aa51c9920e5643668dcb
Alice <nameal1234@gmail.com>
2024-01-04 21:36:12 -0600

    add jquery and mustache

--- js/helpers.js
+++ js/helpers.js
@@ -4,10 +4,10 @@ function gradeTest() {
     const rwEH = document.getElementById('rwEH').value;
     const mathEH = document.getElementById('mathEH').value;
     const rows = textareaValue.trim().split('\n');
-    const rw = [];
-    const rw2 = [];
-    const math = [];
-    const math2 = [];
+    const studentRW = [];
+    const studentRW2 = [];
+    const studentMath = [];
+    const studentMath2 = [];
 
     //set localstorage stuff
     console.log("setting items for test " + testNum + "with sections: " + rwEH + " " + mathEH);
@@ -15,65 +15,34 @@ function gradeTest() {
     localStorage.setItem('rw2', JSON.stringify(window['rw'+rwEH+'Questions'+testNum]));
     localStorage.setItem('math1', JSON.stringify(window['mathQuestions'+testNum]));
     localStorage.setItem('math2', JSON.stringify(window['math'+mathEH+'Questions'+testNum]));
-    console.log("reading:");
-    console.log(JSON.parse(localStorage.getItem('rw1')));
-    console.log("rw2:");
-    console.log(JSON.parse(localStorage.getItem('rw2')));
-    console.log("math:");
-    console.log(JSON.parse(localStorage.getItem('math1')));
-    console.log("math2:");
-    console.log(JSON.parse(localStorage.getItem('math2')));
+
     //extract vals into cols
-    // rows.forEach(row => {
-    //     const columns = row.split(' ');
-    //     rw.push(columns[0]);
-    //     rw2.push(columns[1]);
-    //     math.push(columns[2]);
-    //     math2.push(columns[3]);
-    // });
+    rows.forEach(row => {
+        const columns = row.split(' ');
+        studentRW.push(columns[0]);
+        studentRW2.push(columns[1]);
+        studentMath.push(columns[2]);
+        studentMath2.push(columns[3]);
+    });
 
-    // gradeRW(answers, testNum);
-    // if(rwEH === 'Easy') {
-    //     gradeRWEasy(answers, testNum);
-    // }
-    // else {
-    //     gradeRWHard(answers, testNum);
-    // }
-    // gradeMath(answers, testNum);
-    // if(mathEH === 'Easy') {
-    //     gradeMathEasy(answers, testNum);
-    // }
-    // else {
-    //     gradeMathHard(answers, testNum);
-    // }
+    //set selected-answer in localstorage
+    const rw1 = JSON.parse(localStorage.getItem('rw1')) || { questions: [] };
+    const rw2 = JSON.parse(localStorage.getItem('rw2')) || { questions: [] };
+    const math1 = JSON.parse(localStorage.getItem('math1')) || { questions: [] };
+    const math2 = JSON.parse(localStorage.getItem('math2')) || { questions: [] };
+    for(let i = 1; i < 28; i++) {
+        rw1.questions.find(q => q.question === i)['selected-answer'] = studentRW[i];
+        rw2.questions.find(q => q.question === i)['selected-answer'] = studentRW2[i];
+    }
+    for(let i = 1; i < 23; i++) {
+        math1.questions.find(q => q.question === i)['selected-answer'] = studentMath[i];
+        math2.questions.find(q => q.question === i)['selected-answer'] = studentMath2[i];    
+    }
 
-}
-
-//answers is an array
-//have localstorage set beforehand
-//populate the localstorage items
-//only compile correct/incorrect with student answers for now
-function gradeRW(answers, testNum) {
-    
-}
-
-function gradeRWEasy(answers, testNum) {
-    
-}
-
-function gradeRWHard(answers, testNum) {
-    
-}
-
-function gradeMath(answers, testNum) {
-    
-}
-
-function gradeMathEasy(answers, testNum) {
-    
-}
-
-function gradeMathHard(answers, testNum) {
-    
+    //set updated localstorage
+    localStorage.setItem('rw1', JSON.stringify(rw1));
+    localStorage.setItem('rw2', JSON.stringify(rw2));
+    localStorage.setItem('math1', JSON.stringify(math1));
+    localStorage.setItem('math2', JSON.stringify(math2));
 }
 
